c0e61df3b6e3912baee6da14b3598d798a04c307
Lucas Alava Peña <lucas.alavapena@gmail.com>
2023-03-23 16:53:20 +0100

    Fixed symbolic vertex creation and merged create vertex functions

--- graph.py
+++ graph.py
@@ -29,53 +29,38 @@ def get_edges_vertices(path):
 
     nodes = {path: Vertex(abs_path=path)}
 
-    # TODO: Combine the below two functions
-    def create_relative_vertex(dir_path, file_name):
-        dest_abs = Path(dir_path) / file_name
-
+    def create_vertex(vertex_path: Path):
         # In the case that you have a symbolic link pointing further down
         # in the directory tree, the vertex would already be created
-        if str(dest_abs) in nodes:
-            return nodes[str(dest_abs)]
+        key = str(vertex_path)
+        if key in nodes:
+            return nodes[key]
 
-        dest_rel = dest_abs.relative_to(curr_dir)
-
-        nodes[str(dest_abs)] = Vertex(abs_path=dest_abs, rel_path=dest_rel)
-
-        return nodes[str(dest_abs)]
-
-    def create_symbolic_vertex(resolved_path):
-        dest_abs = Path(resolved_path)
-
-        if str(dest_abs) in nodes:
-            return nodes[str(dest_abs)]
-
-        # in case it points further down
+        dest_rel = vertex_path.relative_to(curr_dir)
+        is_sym = os.path.islink(vertex_path)
         try:
-            dest_rel = dest_abs.relative_to(curr_dir)
-            nodes[str(dest_abs)] = Vertex(abs_path=dest_abs, rel_path=dest_rel, is_sym=True)
+            nodes[key] = Vertex(abs_path=vertex_path, rel_path=dest_rel, is_sym=is_sym)
         except ValueError:
             # In case symlink is outside of the directory dag show would be called on
             #  Path.relative_to raises a ValueError if dest_abs is not a sub directory of curr_dir
-            nodes[str(dest_abs)] = Vertex(abs_path=dest_abs, is_sym=True)
+            nodes[key] = Vertex(abs_path=vertex_path, is_sym=is_sym)
 
-        return nodes[str(dest_abs)]
+        return nodes[key]
 
     for (dir_path, dir_names, file_names) in os.walk(path, followlinks=True):
         parent_vertex = nodes[dir_path]
 
         for file_name in file_names + dir_names:
-            
-            dest_vertex = create_relative_vertex(dir_path, file_name)
+            dest_vertex = create_vertex(Path(dir_path) / file_name)
             edges.append((parent_vertex, dest_vertex))
 
-            if os.path.islink(dest_vertex.abs_path):
+            # Symbolic link case
+            if dest_vertex.is_sym:
                 # In the case of a cycle os.path.realpath appears to resolve to the input path
                 # So it likely we are getting a DAG,
                 # TODO but this need to be tested thorougly
-                resolved_path = os.path.realpath(dest_vertex.abs_path)
-
-                symbolic_dest_vertex = create_symbolic_vertex(resolved_path)
-                edges.append((dest_vertex, symbolic_dest_vertex))
+                resolved_path = Path(os.path.realpath(dest_vertex.abs_path))
+                resolved_vertex = create_vertex(resolved_path)
+                edges.append((dest_vertex, resolved_vertex))
 
     return edges, nodes
